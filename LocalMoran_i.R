f22aca06c27606b9b91dfb3e24a5b402d72029fe
Pol Llagostera Blasco <plb92@hotmail.com>
2022-01-11 15:31:05 +0100

    Corrected name of Getis G (not Gary).
    Added example to calculate the Local Moran-i correlation.

---
+++ LocalMoran_i.R
@@ -0,0 +1,50 @@
+library(igraph)
+
+adj_mtx1 <- matrix(data = c(0,3,4,0, 3,0,5,0, 4,5,0,8, 0,0,8,0), nrow = 4)
+adj_mtx2 <- matrix(data = c(0,3,4,0,9,  3,0,5,0,0,  4,5,0,8,0, 0,0,8,0,0,  9,0,0,0,0), nrow = 5)
+adj_mtx3 <- matrix(data = c(0,1,1,0, 1,0,0,1, 1,0,0,1, 0,1,1,0), nrow = 4 )
+
+adj_mtx <- adj_mtx3
+
+g_w <- graph_from_adjacency_matrix(adj_mtx, mode = 'undirected', weighted = TRUE)
+plot(g_w, e=TRUE, v=TRUE)
+
+# Add node weights
+g_w <- set_vertex_attr(g_w, name='intensity', valu=c(5,10,15,20))
+
+adj_mtx <- igraph::as_adj(graph = g_w)
+adj_listw <- spdep::mat2listw(adj_mtx)
+nb <- adj_listw$neighbours
+
+w_listw <- spdep::nb2listw(nb, style="W", zero.policy=TRUE) 
+
+intensity_e <- igraph::edge_attr(g_w, 'weight')
+intensity_v <-  igraph::vertex_attr(g_w, 'intensity')
+intensity <- intensity_v
+
+locmoran <- spdep::localmoran_perm(x = intensity, 
+                                   listw = w_listw, 
+                                   zero.policy = TRUE, 
+                                   na.action = na.omit, 
+                                   nsim = 999) # 999 permutations
+
+# Calculate deviations
+node_int_deviation <- intensity - mean(intensity)  
+locmoran_deviation <- locmoran[, 'Ii'] - mean(locmoran[, 'Ii'])
+
+# create a new variable identifying the moran plot quadrant for each observation, dismissing the non-significant ones
+quad_sig <- NA
+significance <- 0.05
+
+# non-significant observations
+quad_sig[(locmoran[, 5] > significance)] <- 2 # "insignificant"  
+# low-low quadrant
+quad_sig[(node_int_deviation < 0 & locmoran_deviation < 0) & (locmoran[, 5] <= significance)] <- 3 # "low-low"
+# low-high quadrant
+quad_sig[(node_int_deviation < 0 & locmoran_deviation > 0) & (locmoran[, 5] <= significance)] <- 4 # "low-high"
+# high-low quadrant
+quad_sig[(node_int_deviation > 0 & locmoran_deviation < 0) & (locmoran[, 5] <= significance)] <- 5 # "high-low"
+# high-high quadrant
+quad_sig[(node_int_deviation > 0 & locmoran_deviation > 0) & (locmoran[, 5] <= significance)] <- 6 # "high-high"
+
+quad_sig[setdiff(as.numeric(igraph::V(g_w)), igraph::V(g_w))] <- 1 # Not contemplated vertices 
