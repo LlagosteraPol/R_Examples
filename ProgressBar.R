6c0ceb16fbb97da04c5cf6bb06068301cccfa70c
Pol Llagostera Blasco <plb92@hotmail.com>
2022-03-28 16:00:06 +0200

    small fix

--- ProgressBar.R
+++ ProgressBar.R
@@ -5,7 +5,7 @@ testit <- function(x = sort(runif(20)), ...)
   pb <- utils::txtProgressBar(...)
   for(i in c(0, x, 1)) {Sys.sleep(0.5); setTxtProgressBar(pb, i)}
   Sys.sleep(1)
-  utils::close(pb)
+  close(pb)
 }
 testit()
 testit(runif(10))
