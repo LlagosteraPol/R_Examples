50a95e7a970fd532b2be1dfb6d0e525443de1422
Pol Llagostera Blasco <plb92@hotmail.com>
2022-06-15 20:26:33 +0200

    Added example to transform a shp file to an intensitynet object.

---
+++ shp2intensitynet.R
@@ -0,0 +1,29 @@
+library(intensitynet)
+library(shp2graph) #Load library
+data(ORN) #Load the Ontario road data from the shp2graph package
+
+rtNEL <- shp2graph::readshpnw(ORN.nt, ELComputed=TRUE) 
+
+nodes_orn <- rtNEL[[2]] 
+edges_orn <- rtNEL[[3]] 
+lengths_orn <- rtNEL[[4]] #edge lengths
+
+
+net_orn<-shp2graph::nel2igraph(nodelist = nodes_orn,
+                               edgelist = edges_orn,
+                               weight = lengths_orn)
+
+
+#Extract the network adjacency matrix
+adj_mtx_orn <- as.matrix(igraph::as_adjacency_matrix(net_orn))
+
+
+#Extract the network node coordinates
+node_coords_orn <-  shp2graph::Nodes.coordinates(nodes_orn)
+
+intnet_orn <- intensitynet(adjacency_mtx = adj_mtx_orn, 
+                           node_coords = node_coords_orn, 
+                           event_data = matrix(ncol = 2))
+
+#We can plot the network to see if the data is correct
+PlotHeatmap(intnet_orn) 
